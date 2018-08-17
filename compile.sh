1ad468e614d098c410574a49ebfe290c0c493578
Tim Armstrong <tarmstrong@cloudera.com>
2018-08-17 16:14:43 -0700

    Fixes to compile and run against CDH6

--- compile.sh
+++ compile.sh
@@ -1,8 +1,7 @@
-export JAVA_HOME="/usr/lib/jvm/java-6-sun/"
-export HADOOP_SRC_HOME="./hadoop-hdfs/"
-export HADOOP_HOME="/usr/lib/hadoop/" # CDH
-export CXXFLAGS="-O2 -g -Wall -I$HADOOP_SRC_HOME/src/c++/libhdfs/ -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/"
-export LDFLAGS="-L$HADOOP_SRC_HOME/build/c++/lib/ -lhdfs -L$JAVA_HOME/jre/lib/amd64/server/ -ljvm"
-export LD_LIBRARY_PATH="$HADOOP_SRC_HOME/build/c++/lib/:$JAVA_HOME/jre/lib/amd64/server/"
+#!/bin/bash
+. $(dirname $0)/config.sh
 
-g++ $CXXFLAGS $LDFLAGS test.cpp
+export CXXFLAGS="-O2 -g -Wall -I$HADOOP_HOME/include/ -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/"
+export LDFLAGS="-L$HADOOP_HOME/lib/native -lhdfs -L$JAVA_HOME/jre/lib/amd64/server/ -ljvm"
+
+g++ test.cpp $CXXFLAGS $LDFLAGS -o test
---
+++ config.sh
@@ -0,0 +1,5 @@
+# Sourced from other shell scripts.
+
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/"
+export HADOOP_HOME=${HADOOP_HOME:-"/home/tarmstrong/Impala/incubator-impala/toolchain/cdh_components-506967/hadoop-3.0.0-cdh6.x-SNAPSHOT/"}
+
